10bef14f00e90cc18b692b6b597e1f981cc6da3a
Gabriel Cesário <gfigueiredocesario@gmail.com>
2025-04-09 08:30:15 -0300

    Delete prjGAARFH/prjGAARFH directory

--- prjGAARFH/prjGAARFH/view/js/checkout.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('checkoutForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Evita o envio do formulário padrão
-
-    // Aqui você pode adicionar a lógica para processar o checkout
-    // Vou apenas redirecionar para uma página de confirmação neste exemplo
-    window.location.href = 'confirmacao.html';
-});
